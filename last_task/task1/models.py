cf59e3f6615edc3fefeff1d2460547a2c4cb27fc
Timur Mursalimov <ladnoladno477@gmail.com>
2024-12-31 10:43:58 +0500

    task19.2

---
+++ last_task/task1/models.py
@@ -0,0 +1,25 @@
+from django.db import models
+from django.db.models import (
+    CharField, DecimalField, 
+    IntegerField, TextField, BooleanField, 
+    ManyToManyField)
+
+# Create your models here.
+class Buyer(models.Model):
+    name = CharField(max_length=50)
+    balance = DecimalField(max_digits=10, decimal_places=3)
+    age = IntegerField(max_length=3)
+    
+    def __str__(self):
+        return self.name
+    
+class Game(models.Model):
+    title = CharField(max_length=100)
+    cost = DecimalField(max_digits=15, decimal_places=3)
+    size = DecimalField(max_digits=15, decimal_places=3)
+    description = TextField()
+    age_limit = BooleanField(default=False)
+    buyer = ManyToManyField(Buyer, related_name='games')
+    
+    def __str__(self):
+        return self.title
