6505e85fdb2ba29c8194a4c6d610a75668744019
Timur Mursalimov <ladnoladno477@gmail.com>
2025-01-07 18:32:38 +0500

    task 19.6

--- last_task/last_task/settings.py
+++ last_task/last_task/settings.py
@@ -75,10 +75,23 @@ WSGI_APPLICATION = 'last_task.wsgi.application'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+
+'default': {
+
+'ENGINE': 'django.db.backends.postgresql',
+
+'NAME': 'postgres', # Имя базы данных
+
+'USER': 'postgres', # Пользователь PostgreSQL
+
+'PASSWORD': 'pass', # Пароль PostgreSQL
+
+'HOST': 'localhost', # Адрес сервера базы данных
+
+'PORT': '5432', # Порт подключения
+
+}
+
 }
 
 
